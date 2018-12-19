fe0ce672305cc5217850d2f5f56dd9534b2e763a
Nasko Vasilev <nasko.vasilev@betvictor.com>
2018-12-19 17:14:03 +0200

    Day 5 Submission

---
+++ _5/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // Some already declared and initialized variables (Task's specific)
+    // Constraints: 2 <= n <= 20
+    let n : i32 = 2;
+
+    for i in 1..11 {
+        println!("{} x {} = {}", n, i, n*i);
+    }
+}

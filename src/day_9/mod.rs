f3383448e1155cf7896b870bc699eaa8c700361b
Nasko Vasilev <magiccrafter@gmail.com>
2018-12-25 15:50:03 +0200

    Day 9 Submission

---
+++ src/day_9/mod.rs
@@ -0,0 +1,16 @@
+use super::util;
+
+pub fn solution() {
+    let expected_results = util::read_expected_results("9");
+    let (n, lines) = util::read_input_lines("9");
+
+    let num: usize = expected_results.get(0).unwrap().parse().unwrap();
+    assert_eq!(num, factorial(n));
+}
+
+fn factorial(n: usize) -> usize {
+    if n == 1 {
+        return 1;
+    }
+    n * factorial(n-1)
+}
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod day_5;
 mod day_6;
 mod day_7;
 mod day_8;
+mod day_9;
 mod util;
 
 fn main() {
@@ -19,4 +20,5 @@ fn main() {
     day_6::solution();
     day_7::solution();
     day_8::solution();
+    day_9::solution();
 }
